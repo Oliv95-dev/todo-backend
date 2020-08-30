008b1c3e8274fe2e47893a9dd59cb2d4091b9c31
Oliv95 <sovaoliv@gmail.com>
2020-08-30 20:02:50 +0300

    mycommt300820

---
+++ api/routes/index.js
@@ -0,0 +1,9 @@
+
+import auth from "../controllers/authController";
+import validateAuth from '../middlewares/auth';
+
+export const routes = (app) => {
+    app.get('/',(req,res) => res.send({message: 'Welcome to Todo API'}));
+    app.post('/api/auth/sign_up', validateAuth, auth.signUp);
+    app.post('/api/auth/sign_in', auth.signIn);
+};
